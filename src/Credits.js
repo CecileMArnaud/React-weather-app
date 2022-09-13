9de79ef512c6deb4a1c97bbf9d6c20dc03337c63
CecileMArnaud <104260276+CecileMArnaud@users.noreply.github.com>
2022-09-13 17:34:21 +0200

    Update Credits.js

--- src/Credits.js
+++ src/Credits.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Credits() {
 	return (
 		<div className="row">
-			<a href="https://github.com/CecileMArnaud/Weather-app">Open source </a>
+			<a href="https://github.com/CecileMArnaud/React-weather-app">
+				Open source{" "}
+			</a>
 			by Cecile M Arnaud
 		</div>
 	);
