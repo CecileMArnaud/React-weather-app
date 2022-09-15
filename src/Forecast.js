929062bd72327c2a4f40b39dbea71c9e1dfd91db
CecileMArnaud <104260276+CecileMArnaud@users.noreply.github.com>
2022-09-15 16:50:13 +0200

    forecast day layout

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,21 @@
 import React from "react";
+import WeatherIcon from "react-icons-weather";
 
 export default function Forecast() {
-	return <div id="forecast"></div>;
+	return (
+		<div className="forecast">
+			<div className="forecast-day">Thu</div>
+			<WeatherIcon
+				name="owm"
+				iconId={800}
+				flip="horizontal"
+				rotate="90"
+				className="forecast-weather-icon"
+			/>
+			<div className="forecast-temp">
+				<span className="forecast-temp-max">19˚C</span>
+				<span className="forecast-temp-min">10˚C</span>
+			</div>
+		</div>
+	);
 }
