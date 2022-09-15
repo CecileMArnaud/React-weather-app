a5900b16e7a8fef7779d647b825d7edc643d4eb6
CecileMArnaud <104260276+CecileMArnaud@users.noreply.github.com>
2022-09-15 16:04:56 +0200

    added Fahrenheit conversion and CSS

--- src/MainData.js
+++ src/MainData.js
@@ -1,32 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import FormattedDate from "./FormattedDate";
 
 export default function MainData(props) {
-	return (
-		<div className="col-7 main-info">
-			<h2>
-				<span id="city-name">
-					{props.data.city}
-					<i className="fa-solid fa-spinner fa-spin-pulse"></i>
-				</span>
-			</h2>
-			<div id="today-date">
-				<FormattedDate date={props.data.date} />
+	let [unit, setUnit] = useState("celsius");
+
+	function showFahrenheit(event) {
+		event.preventDefault();
+		setUnit("fahrenheit");
+	}
+
+	function showCelsius(event) {
+		event.preventDefault();
+		setUnit("celsius");
+	}
+	if (unit === "celsius") {
+		return (
+			<div className="col-7 main-info">
+				<h2>
+					<span id="city-name">
+						{props.data.city}
+						<i className="fa-solid fa-spinner fa-spin-pulse"></i>
+					</span>
+				</h2>
+				<div id="today-date">
+					<FormattedDate date={props.data.date} />
+				</div>
+				<div>
+					<span className="main main-temperature">
+						{Math.round(props.data.temperature)}
+					</span>
+					<span className="units">
+						˚C |
+						<a href="/" onClick={showFahrenheit}>
+							˚F
+						</a>
+					</span>
+				</div>
 			</div>
-			<div>
-				<span className="main main-temperature">
-					{Math.round(props.data.temperature)}
-				</span>
-				<span id="units">
-					<a href="/" id="celsius-link" className="active">
-						˚C
-					</a>{" "}
-					|
-					<a href="/" id="fahrenheit-link">
-						˚F
-					</a>
-				</span>
+		);
+	} else {
+		return (
+			<div className="col-7 main-info">
+				<h2>
+					<span id="city-name">
+						{props.data.city}
+						<i className="fa-solid fa-spinner fa-spin-pulse"></i>
+					</span>
+				</h2>
+				<div id="today-date">
+					<FormattedDate date={props.data.date} />
+				</div>
+				<div>
+					<span className="main main-temperature">
+						{Math.round((props.data.temperature * 9) / 5 + 32)}
+					</span>
+					<span className="units">
+						˚F |{" "}
+						<a href="/" onClick={showCelsius}>
+							˚C
+						</a>
+					</span>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
